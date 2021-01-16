6914354d1c932f913eb991c6915993074a4f8c7a
unknown <robert.victor.muresan@gmail.com>
2021-01-16 09:37:51 -0500

    Working

--- popup.js
+++ popup.js
@@ -1,120 +1,9 @@
-/**
- * Copyright 2018 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-function turnOffInferring() {
-  document.getElementById('infer').checked = false;
-  chrome.storage.local.set({
-    'infer': false
-  }, () => {});
-  chrome.extension.sendRequest({
-    infer: false
-  });
-}
-
-function prepareNoActionTrain() {
-  chrome.extension.sendRequest({
-    train: 'off'
-  });
-  chrome.extension.sendRequest({
-    reset: true
-  });
-  document.querySelector('#traintext').innerHTML =
-    'Training no action in 2 seconds';
-  setTimeout(startNoActionTrain, 2000);
-}
-
-function startNoActionTrain() {
-  document.querySelector('#traintext').innerHTML =
-    'Training no action for 5 seconds';
-  chrome.extension.sendRequest({
-    train: 'noaction'
-  });
-  setTimeout(prepareScrollDownTrain, 5000);
-}
-
-function prepareScrollDownTrain() {
-  chrome.extension.sendRequest({
-    train: 'off'
-  });
-  document.querySelector('#traintext').innerHTML =
-    'Training scroll down in 2 seconds';
-  setTimeout(startScrollDownTrain, 2000);
-}
-
-function startScrollDownTrain() {
-  document.querySelector('#traintext').innerHTML =
-    'Training scroll down for 5 seconds';
-  chrome.extension.sendRequest({
-    train: 'down'
-  });
-  setTimeout(prepareScrollUpTrain, 5000);
-}
-
-function prepareScrollUpTrain() {
-  chrome.extension.sendRequest({
-    train: 'off'
-  });
-  document.querySelector('#traintext').innerHTML =
-    'Training scroll up in 2 seconds';
-  setTimeout(startScrollUpTrain, 2000);
-}
-
-function startScrollUpTrain() {
-  document.querySelector('#traintext').innerHTML =
-    'Training scroll up for 5 seconds';
-  chrome.extension.sendRequest({
-    train: 'up'
-  });
-  setTimeout(finishTraining, 5000);
-}
-
-function finishTraining() {
-  document.querySelector('#traintext').innerHTML = '';
-  chrome.extension.sendRequest({
-    train: 'save'
-  });
-  document.getElementById('infer').checked = true;
-  chrome.storage.local.set({
-    'infer': true
-  }, () => {});
-  chrome.extension.sendRequest({
-    infer: true
-  });
-}
-
-function inferButtonClicked() {
-  const inferSetting = this.checked;
-  chrome.storage.local.set({
-    'infer': inferSetting
-  }, () => {});
-  chrome.extension.sendRequest({
-    infer: inferSetting
-  });
-}
-
-function trainClicked() {
-  turnOffInferring();
-  prepareNoActionTrain();
-}
-
 function setupCam() {
   navigator.mediaDevices.getUserMedia({
     video: true
   }).then(mediaStream => {
     document.querySelector('#webcamVideo').srcObject = mediaStream;
+    track_face()
   }).catch((error) => {
     console.warn(error);
   });
@@ -122,9 +11,55 @@ function setupCam() {
 
 setupCam();
 
-// Setup checkbox with correct initial value.
-chrome.storage.local.get('infer', items =>
-  document.getElementById('infer').checked = !!items['infer']);
 
-document.getElementById('infer').onclick = inferButtonClicked;
-document.getElementById('train').onclick = trainClicked;
+
+async function track_face() {
+  await tf.setBackend('webgl');
+  console.log(tf.getBackend());
+  let blinks = 0;
+  // const canvas = document.getElementById("canv");
+  const frame = document.getElementById("webcamVideo");
+  const counter= document.getElementById("counter")
+
+  // Load the MediaPipe facemesh model.
+  const model = await faceLandmarksDetection.load(faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
+  const video = document.querySelector("video");
+
+  
+  var last_blinked = 0;
+  while(true){
+      var faces = await model.estimateFaces({ input: video });
+
+      
+      faces.forEach(face => {
+          const start = face.boundingBox.topLeft;
+          const end = face.boundingBox.bottomRight;
+          const size = [end[0] - start[0], end[1] - start[1]];
+
+
+
+          var mesh = face.mesh;
+          const left = [-1,33,160,158,133,153,144]
+          var EAR_LEFT = (Math.abs(mesh[left[2]][0]-mesh[left[6]][0]) + Math.abs(mesh[left[3]][0] -mesh[left[5]][0]))/(2 * Math.abs(mesh[left[1]][1]- mesh[left[4]][1]))
+          const right = [-1,362,385,387,263,373,380]
+          var EAR_RIGHT = (Math.abs(mesh[right[2]][0]-mesh[right[6]][0]) + Math.abs(mesh[right[3]][0] -mesh[right[5]][0]))/(2 * Math.abs(mesh[right[1]][1]- mesh[right[4]][1]))
+          // console.log(EAR_LEFT)
+          // console.log(EAR_RIGHT)
+          
+          
+          var time_now = new Date().getTime()
+          console.log(time_now + " " + last_blinked)
+          const timeOut = 150;
+          if(EAR_LEFT < 0.8 && EAR_RIGHT < 0.8 && (time_now - last_blinked)>timeOut){
+              blinks++;
+              document.getElementById("counter").innerHTML = "Blinks: " + blinks;
+              last_blinked = new Date().getTime();
+          }
+      });
+      await tf.nextFrame();
+  }
+}
+
+
+
+
